96014dae3e1f50f3b3760c9aac673ff7d57869ff
srinivasanmsk <106843407+srinivasanmsk@users.noreply.github.com>
2023-04-07 13:34:52 +0530

    Added a step install 'curl' pkg

--- install-docker.sh
+++ install-docker.sh
@@ -2,6 +2,10 @@
 
 sudo apt update -y
 
+sudo apt install curl -y
+
+sudo apt update -y
+
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
